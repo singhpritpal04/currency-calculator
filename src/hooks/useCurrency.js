d62b3a289fb60a76aac2e80fada98e1987a316f1
SINGHPRITPAL04 <singhpritpal2000@hotmail.com>
2024-09-12 13:47:00 +0000

    update 1

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -3,5 +3,6 @@ import {useState, useEffect}  from 'react'
 function useCurrency(currency) {
     useEffect({
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then
     },[])
 }
